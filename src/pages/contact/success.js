993a00b9357d089853c274b3224d3d29c300f337
Ethan Olsen <131eolsen@gmail.com>
2020-10-09 15:33:04 -0700

    netlify form bot field

--- src/pages/contact/success.js
+++ src/pages/contact/success.js
@@ -22,14 +22,12 @@ const ContactSuccess = () => {
 	const { markdownRemark } = data
 	const color = markdownRemark.frontmatter.color
 	const success = markdownRemark.frontmatter.success
+	const { width, height } = useWindowSize()
 
 	return (
 		<Layout color={color}>
 			<SEO title='Contact' />
-			<Confetti
-				width={window !== undefined ? window.innerWidth : 200}
-				height={window !== undefined ? window.innerHeight : 200}
-			/>
+			<Confetti width={width} height={height} />
 			<ContactContainer>
 				<ContactContent>
 					<h1>{success}</h1>
@@ -37,6 +35,38 @@ const ContactSuccess = () => {
 			</ContactContainer>
 		</Layout>
 	)
+}
+
+// Hook
+function useWindowSize() {
+	// Initialize state with undefined width/height so server and client renders match
+	// Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+	const [windowSize, setWindowSize] = useState({
+		width: undefined,
+		height: undefined,
+	})
+
+	useEffect(() => {
+		// Handler to call on window resize
+		function handleResize() {
+			// Set window width/height to state
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			})
+		}
+
+		// Add event listener
+		window.addEventListener('resize', handleResize)
+
+		// Call handler right away so state gets updated with initial window size
+		handleResize()
+
+		// Remove event listener on cleanup
+		return () => window.removeEventListener('resize', handleResize)
+	}, []) // Empty array ensures that effect is only run on mount
+
+	return windowSize
 }
 
 const ContactContainer = styled(motion.div)`
